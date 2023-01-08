99928aecdb3899bb55dd90e3cd825478fe89895f
Nikita Pilez <pileznikita99@gmail.com>
2023-01-08 19:20:21 +0300

    Lesson 2

---
+++ lesson_2/index.php
@@ -0,0 +1,51 @@
+<?php
+
+
+
+// Example bool
+//var_dump((bool) "");
+//var_dump((bool) "0");
+//var_dump((bool) 1);
+//var_dump((bool) -5);
+//var_dump((bool) "techmeskills");
+//var_dump((bool) 2.4e5);
+//var_dump((bool) array(18));
+//var_dump((bool) array());
+//var_dump((bool) "false");
+
+//Example integer
+
+//$number = 8.1;
+//var_dump((int)$number);
+
+//$number = true;
+//var_dump((int)$number);
+
+//$number = "test";
+//var_dump((int)$number);
+
+//$number = "12";
+//var_dump((int)$number);
+
+//$number = null;
+//var_dump((int)$number);
+
+//$number = 0124;
+//var_dump($number);
+
+//$number = 0b10110111;
+//var_dump($number);
+
+//$number = 0x1A;
+//var_dump($number);
+
+//$myString = "Я вам скажу так: "I'll be back"";
+
+//$myString = 'First string \n second string';
+
+//$a = 14;
+//$b = 4;
+//
+//$result = $a % $b;
+//
+//print_r($result);
---
+++ lesson_2/plan.md
@@ -0,0 +1,9 @@
+### Типы данных: null, bool, int, string, float, array, object.
+### Приведение типов: в строку, в целое, в бул.
+### Функции is_string() is_int() is_numeric() is_bool() is_array()
+### Функции gettype() empty() isset() 
+### Сравнение типов https://www.php.net/manual/ru/types.comparisons.php
+### Функции сложения, вычитания, умножения, деления, целочисленный остаток от деления
+### Математические функции: abs() ceil() floor() intdiv() max() min() pow() round() https://www.php.net/manual/ru/ref.math.php
+
+#### Дополнительно, преобразование 0124 int, heredoc строка
