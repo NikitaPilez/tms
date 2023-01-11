d3ec72a07c5d27248cd7e09c46234b54d76c64a7
Nikita Pilez <pileznikita99@gmail.com>
2023-01-11 22:57:34 +0300

    Lesson 3

---
+++ lesson_3/example_13.php
@@ -0,0 +1,15 @@
+<?php
+
+// time()
+// date( 'd.m.Y', time());
+
+// mktime (19, 34, 0, 1, 11, 2023);
+//print_r(getdate( ));
+
+print_r(strtotime("now"));
+//echo strtotime("10 September 2000"), "\n";
+//echo strtotime("+1 day"), "\n";
+//echo strtotime("+1 week"), "\n";
+//echo strtotime("+1 week 2 days 4 hours 2 seconds"), "\n";
+//echo strtotime("next Thursday"), "\n";
+//echo strtotime("last Monday"), "\n";
---
+++ lesson_3/example_14.php
@@ -0,0 +1,11 @@
+<?php
+
+$nowDay = date('w');
+
+if ($nowDay < 5 || $nowDay == 7) {
+    print_r(date('d.m.Y', strtotime("+1 day")));
+} else if ($nowDay == 5) {
+    print_r(date('d.m.Y', strtotime("+3 day")));
+} else if ($nowDay == 6) {
+    print_r(date('d.m.Y', strtotime("+2 day")));
+}
