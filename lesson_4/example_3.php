5c4976b86d1cd1f0707ac8308a86df949d002873
Nikita Pilez <pileznikita99@gmail.com>
2023-01-15 21:55:28 +0300

    Lesson 4

--- lesson_4/example_3.php
+++ lesson_4/example_3.php
@@ -7,4 +7,51 @@ $secondArray = ['Nikita', 'Pavel', 'Aleksey'];
 
 $result = array_diff($firstArray, $secondArray);
 
-print_r($result);
+//print_r($result);
+
+
+$array = array_fill(7, 10, 'test');
+//print_r($array);
+
+$keys = array('test_key', 7, 12, 'last_key');
+$array = array_fill_keys($keys, 'password');
+//print_r($array);
+
+function even($number)
+{
+    return $number % 2 == 0;
+}
+$array = [1, 3, 6, 10, 15];
+//print_r(array_filter($array, "even"));
+// if callback empty?
+
+$firstColors = ['green', 'blue', 'red'];
+$secondColors = ['yellow', 'black', 'white', 'green'];
+$result = array_intersect($firstColors, $secondColors);
+//print_r($result);
+
+$elem = [
+    'name' => 'Nik',
+    'position' => 'developer',
+    'phone' => '+375334958675',
+];
+$result = array_key_exists('phone', $elem);
+//print_r($result);
+
+function square($n)
+{
+    return ($n * $n);
+}
+
+$firstArray = [1, 2, 3, 4, 5];
+$resultArray = array_map('square', $firstArray); // можно использовать стрелочную функцию
+//print_r($resultArray);
+
+$firstArray = [1, 3, 5];
+$secondArray = [2, 4, 6];
+$resultArray = array_merge($firstArray, $secondArray);
+//print_r($resultArray);
+
+$fruitsArray = ["orange", "banana"];
+array_push($fruitsArray, "apple", "raspberry");
+//print_r($fruitsArray);
--- lesson_4/plan.md
+++ lesson_4/plan.md
@@ -3,9 +3,9 @@
 Многомерные массивы
 Функции для работы с массивами
 
-основы работы с массивами https://www.php.net/manual/ru/language.types.array.php (1)
-сортировка массива https://www.php.net/manual/ru/array.sorting.php (2)
-функции для работы с массивами array_chunk() array_diff() https://www.php.net/manual/ru/ref.array.php (3)
+### Основы работы с массивами https://www.php.net/manual/ru/language.types.array.php (1)
+### Сортировка массива https://www.php.net/manual/ru/array.sorting.php (2)
+### Функции для работы с массивами https://www.php.net/manual/ru/ref.array.php (3)
 
 
 ### Задание, определить какой день недели был в введеную дату 
