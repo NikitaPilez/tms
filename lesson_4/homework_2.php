683865faf06fc13ba9e6570221cfffa7e25254d4
NikitaPilez <pileznikita99@gmail.com>
2023-01-19 12:28:11 +0300

    Fix

--- lesson_4/homework_2.php
+++ lesson_4/homework_2.php
@@ -1,18 +1,6 @@
 <?php
 
 $string = 'Hello 34 fdgfdg 45 fg 55hhhg556';
-$digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-$array = str_split($string);
-$countDigits = 0;
-$sum = 0;
-
-$digitsCountFunc = function ($item) use ($digits, &$countDigits, &$sum) {
-    if (in_array($item, $digits)) {
-        $countDigits++;
-        $sum += $item;
-    }
-};
-
-array_map($digitsCountFunc, $array);
-
-print_r('Количество ' . $countDigits . ' сумма ' . $sum);
+$stringArray = str_split($string);
+$filterArray = array_filter($stringArray, 'is_numeric');
+print_r('Количество ' . count($filterArray) . ' сумма ' .  array_sum($filterArray));
