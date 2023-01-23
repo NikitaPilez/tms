7049bafc853e6f7c74382ea1804d2a9e95fa94a9
NikitaPilez <pileznikita99@gmail.com>
2023-01-23 17:25:19 +0300

    Lesson 6

--- lesson_6/example_15.php
+++ lesson_6/example_15.php
@@ -4,6 +4,9 @@
 
 // 'A(?=[-_a-zA-Z0-9]*?[A-Z])(?=[-_a-zA-Z0-9]*?[a-z])(?=[-_a-zA-Z0-9]*?[0-9])[-_a-zA-Z0-9]{6,}z'
 
-$password = 'sdfsdfEdfdfdf6';
-$regex = '/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/';
-print_r(preg_match($regex, $password));
+//$password = 'sdfsdfEdfdfdf6';
+//$regex = '/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/';
+//print_r(preg_match($regex, $password));
+
+$pattern = '/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/';
+print_r(preg_match($pattern, 'test@gmail.com'));
