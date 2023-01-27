f2eeb85302057dc997a26bdb33e94046e4a5ff7c
NikitaPilez <pileznikita99@gmail.com>
2023-01-27 13:12:56 +0300

    Lesson 8

---
+++ lesson_8/example_1.php
@@ -0,0 +1,18 @@
+<?php
+//
+//setcookie('name', 'Nikita', time() + 300);
+//setcookie('name', '', time() - 1);
+//setcookie('age', '23');
+
+//print_r($_COOKIE);
+
+session_start();
+//echo session_id();
+//echo $_COOKIE['PHPSESSID'];
+//print_r($_SESSION['lang']);
+if (!$_SESSION['lang']) {
+    $_SESSION['lang'] = 'En';
+}
+print_r($_SESSION['lang']);
+
+//unset($_SESSION['lang']);
