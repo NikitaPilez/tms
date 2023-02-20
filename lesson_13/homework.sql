67b33275ad2ae6fb880a0b91bf4f812f5e364cef
NikitaPilez <pileznikita99@gmail.com>
2023-02-20 10:08:44 +0300

    Lesson 13

--- lesson_13/homework.sql
+++ lesson_13/homework.sql
@@ -6,3 +6,12 @@
 -- Найти все телефоны моделей Google, iPhone, Samsung
 -- Сделать запрос который выведет среднюю цену всех телефонов и количество таких телефонов сгруппированнах по моделям (использовать alias)
 -- Сделать запрос который группирует телефоны по моделям, при этом сортирует это все по порядку алфавита
+
+SELECT * FROM `products` WHERE count > 0 ORDER BY name;
+SELECT * FROM `products` GROUP BY manufacturer;
+SELECT * FROM `products` WHERE manufacturer = 'iphone' AND ((price > 2000 AND currency = 'BYN')  OR (price > 800 AND currency = 'USD') OR (price > 745 AND currency = 'EUR'));
+SELECT * FROM `products` WHERE name LIKE '%plus%' OR name LIKE '%max%';
+SELECT * FROM `products` WHERE count > 0 AND ((price BETWEEN 1000 and 2000 AND currency = 'BYN') OR (price BETWEEN 370 AND 740 AND currency = 'USD') OR (price BETWEEN 345 AND 690 AND currency = 'EUR'));
+SELECT * FROM `products` WHERE manufacturer IN ('google', 'iphone', 'samsung');
+SELECT SUM(count) AS amount, AVG(price) as average_price, manufacturer FROM `products` GROUP BY manufacturer ORDER BY amount;
+SELECT * FROM `products` GROUP BY manufacturer ORDER BY manufacturer;
